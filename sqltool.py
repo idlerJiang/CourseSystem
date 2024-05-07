540e9b1d7bd28c337ed2ff6d403fa9c68c76fc75
idlerJiang <935365680@qq.com>
2024-05-07 19:14:13 +0800

    查询重做

--- sqltool.py
+++ sqltool.py
@@ -22,28 +22,33 @@ def login(cursor, user_id, password):
 
 
 def query_course(cursor, course_id=None, course_name=None, teacher_id=None, teacher_name=None, time=None):
-    sql = "select course_profile.course_id, course_profile.course_name, course_profile.teacher_id, user_detail.user_name, course_detail.capacity, course_detail.selected, course_detail.time, classroom.location from course_profile, course_detail, classroom, user_detail where true"
+    sql = "select cp.course_id, cp.course_name, cp.teacher_id, ud.user_name, cd.capacity, cd.selected, cd.time, cr.location from course_profile cp join coursesystem.course_detail cd on cp.course_id = cd.course_id and cp.teacher_id = cd.teacher_id join coursesystem.user_detail ud on cd.user_id = ud.user_id join coursesystem.classroom cr on cd.classroom_id = cr.classroom_id where true"
     param = list()
     return_data = list()
     try:
-        if course_id is not None:
-            sql += " and course_id like %s"
+        if course_id is not None and course_id != "":
+            sql += " and cp.course_id like %s"
             param.append("%"+course_id+"%")
-        if course_name is not None:
-            sql += " and course_name like %s"
+        if course_name is not None and course_name != "":
+            sql += " and cp.course_name like %s"
             param.append("%"+course_name+"%")
-        if teacher_id is not None:
-            sql += " and teacher_id like %s"
+        if teacher_id is not None and teacher_id != "":
+            sql += " and cp.teacher_id like %s"
             param.append("%"+teacher_id+"%")
-        if teacher_name is not None:
-            sql += " and teacher_name like %s"
+        if teacher_name is not None and teacher_name != "":
+            sql += " and ud.user_name like %s"
             param.append("%"+teacher_name+"%")
-        if time is not None:
-            sql += " and time like %s"
+        if time is not None and time != "":
+            sql += " and cd.time like %s"
             param.append("%"+time+"%")
+        print(sql, param)
         result = cursor.execute(sql, param)
+        result = cursor.fetchall()
+
+        print("!!!", result)
         if cursor.rowcount > 0:
             for data in result:
+                print("!!!", data)
                 return_data.append(
                     {'course_id': data[0], 'course_name': data[1], 'teacher_id': data[2], 'teacher_name': data[3],
                      'capacity': data[4], 'selected': data[5], 'time': data[6], 'location': data[7]})
